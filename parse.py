8a10b8f5e77ada16e2e5beb25016d2e8002af775
Zeno Zeng <zenoofzeng@gmail.com>
2014-11-05 15:36:09 +0800

    fix index_col

--- parse.py
+++ parse.py
@@ -2,11 +2,11 @@ import pandas
 
 data_23 = pandas.io.excel.read_excel(io='data.xlsx',
                                      sheetname='23',
-                                     index_col=1)
+                                     index_col=0)
 
 data_28 = pandas.io.excel.read_excel(io='data.xlsx',
                                      sheetname='28',
-                                     index_col=1)
+                                     index_col=0)
 print(data_23)
 
 print(data_28)
